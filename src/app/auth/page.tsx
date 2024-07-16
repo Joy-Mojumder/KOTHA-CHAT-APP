382131a07fe076ecd28983d8203672ca9a8bc248
Joy Mojumder Dip <166296999+Joy-Mojumder@users.noreply.github.com>
2024-07-17 01:02:15 +0600

    Adding kotha chat app

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Hello world! It is an auth page
+      </h1>
+    </div>
+  );
+};
+
+export default page;
